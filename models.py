2b811e285d6b95b7d83d301d35870438d0b99a4d
max31ru12 <maxevg72@gmail.com>
2023-12-10 14:37:22 +0300

    Implemented DB-connection, insertion and selection

--- models.py
+++ models.py
@@ -24,8 +24,36 @@ class GetLog(BaseModel):
     log: GetLogData
 
 
-log_entry_data = {"ip": "127.0.0.1", "method": "GET", "uri": "http://example.com", "status_code": 200}
-print(GetLog(**log_entry_data))
+# log_entry_data = {"ip": "127.0.0.1", "method": "GET", "uri": "http://example.com", "status_code": 200}
+valid_data = {
+    "uuid4": "550e8400-e29b-41d4-a716-446655440000",
+    "created": "2023-12-01T12:34:56",
+    "log": {
+        "ip": "192.168.1.1",
+        "method": "GET",
+        "uri": "http://example.com",
+        "status_code": 200
+    }
+}
+
+
+# aaaa = {
+#     "ip": "192.168.1.1",
+#     "method": "GET",
+#     "uri": "https://example.com/path/to/resource",
+#     "status_code": 200
+# }
+
+try:
+    print(GetLogData(**{
+                        "ip": "192.16",
+                        "method": "str",
+                        "uri": "URI",
+                        "status_code": "int"
+                    }))
+except:
+    print("hello")
+
 
 
 # Валидация такая вот
--- web.py
+++ web.py
@@ -1,35 +1,79 @@
-from fastapi import APIRouter
-from models import PostLog
+from fastapi import APIRouter, FastAPI, HTTPException, status
+from models import PostLog, GetLog, GetLogData
+import psycopg2
+import uuid
 
 
-app = APIRouter()
+app = FastAPI()
+app_router = APIRouter()
+
+# Подклбчение к бд Postgres
+conn = psycopg2.connect(dbname="mtech",
+                        user="mtech",
+                        password="mtech_321")
+cur = conn.cursor()
 
 
-data_list = []
-
-
-
-
-
-@app.get("/api/data")
+@app_router.get("/api/data")
 async def get_data() -> list:
-    return data_list
+    try:
+        query = """SELECT uuid4, created, ip, method, uri, status_code 
+                   FROM get_log AS A INNER JOIN get_log_data AS B ON A.log_id = B.id;
+                """ 
+        cur.execute(query)
+        data = cur.fetchall()
+        return_list = []
+        for i in data:
+            data_dict = {}
+            data_dict["id"] = i[0]
+            # Дату надо привести в нормальный вид
+            data_dict["created"] = i[1]
+            data_dict["log"] = {
+                "ip": i[2],
+                "method": i[3],
+                "uri": i[4],
+                "status_code": i[5],
+            }
+            return_list.append(data_dict)
+        
+    except:
+        pass
+    return return_list
 
 
 
-@app.post("/api/data")
+@app_router.post("/api/data", status_code=201)
 async def post_data(log: PostLog) -> dict:
 
+    try:
+        # Парсинг
+        log = str(log)[5:].replace("'", "").split()
+        # Запись в словарь
+        log = {"ip": log[0][1:-1],
+            "method": log[1][1:-1],
+            "uri": log[2][1:-1],
+            "status_code": log[3][1:-1]}
+        
+        # Валидация
+        GetLogData(**log)
+    
+        # Вставляю в лог
+        cur.execute("""INSERT INTO get_log_data (ip, method, uri, status_code)
+                        VALUES (%s, %s, %s, %s) RETURNING id;""", 
+                        (log["ip"], log["method"], log["uri"], log["status_code"]))
+        inserted_id = int(cur.fetchone()[0])
+        cur.execute("""INSERT INTO get_log (uuid4, log_id) VALUES (%s, %s);""", (str(uuid.uuid4()), inserted_id))
+
+        conn.commit()
+        
+        # Тут будет добавление в базу данных
+        return {"message": "succesfully added data"}
+        
+    except:
+        raise HTTPException(status_code=status.HTTP_418_IM_A_TEAPOT, detail="Что-то пошло не так")
     
 
-    log = str(log)[5:].replace("'", "").split()
-
-    log = {"ip": log[0][1:-1],
-           "method": log[1][1:-1],
-           "uri": log[2][1:-1],
-           "status_code": log[3][1:-1]}
-
-    data_list.append(log)
 
 
-    return {"message": "succesfully added data"}
+
+app.include_router(app_router)
