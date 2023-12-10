738ce468e1116c13f0b6b132a32c4d15770abe36
max31ru12 <maxevg72@gmail.com>
2023-12-10 23:52:49 +0300

    Fixed delay-time

--- client.py
+++ client.py
@@ -1,7 +1,14 @@
 import random
 import requests
-import json
-import logging
+import threading
+import os
+from dotenv import load_dotenv
+import time
+
+# Работа с переменными окружения
+load_dotenv()
+thred_quantity = int(os.getenv("thread_quantity"))
+request_delay = int(os.getenv("request_delay"))
 
 # """В задании буквально написано, что строка выглядит следующим образом: '{208.33.245.18} {PUT} {http://my-example.com} {108}' """
 
@@ -21,6 +28,7 @@ def generate_request_data() -> str:
 
     address = '.'.join([str(random.randint(1, 256)) if i == 0 else str(random.randint(0, 256)) for i in range(4)])
     method = random.choice(responses)
+    # Сделать генерацию рандомного адреса?
     uri = 'http://my-example.com'
     status_code = random.randint(100, 600)
 
@@ -35,20 +43,19 @@ def send_post_request(data: generate_request_data) -> None:
     try:
         request_data = {"log": data}
         response = requests.post("http://127.0.0.1:8000/api/data", json=request_data)
-        # Может быть логировать надо в другом месте???
-        log(request_data)
+        # Данные логируются только в случае успеха отправки данных
+        log(data)
         print(response.status_code)
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
 
-generate_request_data()
+while True:
+    for i in range(thred_quantity):
 
+        thread = threading.Thread(target=send_post_request, args=(generate_request_data(), ), daemon=True)
+        thread.start()
+        thread.join()
 
-
-
-
-
-
-
+    time.sleep(random.uniform(0, request_delay / 1000.0))
